c669337bc1117cc2c6a89a5110a198afc38d133c
anaelhoffmann2009 <158530923+anaelhoffmann2009@users.noreply.github.com>
2024-02-06 18:40:45 -0300

    Update diccionario de palabras .py
    Created by Anael Hoffmann

--- diccionario de palabras .py
+++ diccionario de palabras .py
@@ -1,18 +1,24 @@
-
-meme_dict = {
-            "CRINGE": "algo raro o embarazoso",
-            "LOL": "una respuesta a algo gracioso",
-            "ROFL":"una respuesta a una broma",
-            "SHEESH":"ligera desaprobación",
-            "CREEPY":"aterrador, siniestro",
-            "AGGRO":"ponerse agresivo/enojado",
-            "MC":"minecraft",
-            "CAUSA":"es como decir amigo",
-            }
-word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
-if word in meme_dict.keys():
-    # ¿Qué debemos hacer si se encuentra la palabra?
-    print(meme_dict[word])
-else:
-    # ¿Qué hacer si no se encuentra la palabra?
-    print("esa palabra no esta en este diccionario")
+# Recomendación: Poner el código en inglés para que más personas de todo el mundo puedan contribuir
+class Dictionary():
+            def __init__(self):
+                        self.meme_dict = {
+                        "CRINGE": "algo raro o embarazoso",
+                        "LOL": "una respuesta a algo gracioso",
+                        "ROFL":"una respuesta a una broma",
+                        "SHEESH":"ligera desaprobación",
+                        "CREEPY":"aterrador, siniestro",
+                        "AGGRO":"ponerse agresivo/enojado",
+                        "MC":"minecraft",
+                        "CAUSA":"es como decir amigo",
+                        }
+                        self.search_in_Dictionary()
+            # Function to search words that is on the dictionary
+            def search_in_Dictionary(self):
+                        word = str(input("Write a word that you don't understand (with capital letters): "))
+                        for i in range(8):
+                                    if word in self.meme_dict:
+                                                print(self.meme_dict[word])
+                                    else:
+                                                print("Error! The word is not in dictionary, please, write other word")
+            
+dictionary = Dictionary()
